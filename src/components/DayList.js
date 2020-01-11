0bc3089854b65f259ee0c75cd3d4e58ce13899b8
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-10 21:51:19 -0500

    created DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import DayListItem from 'components/DayListItem';
+
+export default function DayList(props) {
+
+  let dayList = props.days.map(day => (
+        <DayListItem key={day._id}
+                     name={day.name}
+                     spots={day.spots}
+                     selected={day.name === props.day}
+                     setDay={props.setDay}/>
+  ));
+
+  return (
+    <ul>
+      {dayList}
+    </ul>
+  );
+}
