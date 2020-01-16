f3209ac60f2daa291998ae0069b846fecf404d38
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-16 14:37:59 -0500

    added stories for InterviwerItemList

--- stories/index.js
+++ stories/index.js
@@ -5,27 +5,29 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
-// import Button from "components/Button";
+import Button from "components/Button";
 import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
+import InterviewerListItem from 'components/InterviewerListItem';
+import InterviewerList from 'components/InterviewerList';
 
-// storiesOf("Button", module)
-//   .addParameters({
-//     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-//   })
-//   .add("Base", () => <Button>Base</Button>)
-//   .add("Confirm", () => <Button confirm>Confirm</Button>)
-//   .add("Danger", () => <Button danger>Cancel</Button>)
-//   .add("Clickable", () => (
-//     <Button onClick={action("button-clicked")}>Clickable</Button>
-//   ))
-//   .add("Disabled", () => (
-//     <Button disabled onClick={action("button-clicked")}>
-//       Disabled
-//     </Button>
-//   ));
+storiesOf("Button", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Base", () => <Button>Base</Button>)
+  .add("Confirm", () => <Button confirm>Confirm</Button>)
+  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Clickable", () => (
+    <Button onClick={action("button-clicked")}>Clickable</Button>
+  ))
+  .add("Disabled", () => (
+    <Button disabled onClick={action("button-clicked")}>
+      Disabled
+    </Button>
+  ));
 
-  storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
+ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   }) // Provides the default background color for our component
@@ -99,5 +101,28 @@ storiesOf("InterviewerListItem", module)
     />
   ));
 
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
 
-
+storiesOf("InterviewerList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Initial", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      setInterviewer={action("setInterviewer")}
+    />
+  ))
+  .add("Preselected", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      interviewer={3}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
