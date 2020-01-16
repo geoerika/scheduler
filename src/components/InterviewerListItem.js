111c939cbe923f8980f0299563504d86c0c2570a
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-16 14:37:31 -0500

    finished component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,14 +5,19 @@ let classNames = require('classnames');
 
 export default function InterviewerListItem(props) {
 
+  let interviewerClass=classNames({
+    'interviewers__item': true,
+    'interviewers__item--selected': props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
+        src={props.avatar}
         alt="Sylvia Palmer"
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 }
