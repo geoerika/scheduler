cd937935b3f2ebd756cd3f7a3f42b77f48e47cee
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-16 12:42:48 -0500

    deleted underscore for id

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,7 @@ import DayListItem from 'components/DayListItem';
 export default function DayList(props) {
 
   let dayList = props.days.map(day => (
-        <DayListItem key={day._id}
+        <DayListItem key={day.id}
                      name={day.name}
                      spots={day.spots}
                      selected={day.name === props.day}
