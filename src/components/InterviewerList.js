89ee26423af03f3455e84810917f3f50db5612e7
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-16 20:18:09 -0500

    created InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import InterviewerListItem from 'components/InterviewerListItem';
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+
+  let interviewerList = props.interviewers.map(interviewer => (
+        <InterviewerListItem key={interviewer.id}
+                     name={interviewer.name}
+                     selected={interviewer.id === props.interviewer}
+                     avatar={interviewer.avatar}
+                     setInterviewer={props.setInterviewer}/>
+  ));
+
+  return (
+    <section>
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewerList}
+      </ul>
+    </section>
+
+  );
+}
