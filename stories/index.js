5c1c86059aad8c6dd4d138fa60460750ac559fff
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-17 15:47:40 -0500

    added stories for Appointment component

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 
+import Appointment from 'components/Appointment/index.js';
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -126,3 +128,15 @@ storiesOf("InterviewerList", module)
       onChange={action("onChange")}
     />
   ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with time", () => (
+    <Appointment
+      time="12pm"
+    />
+  ))
+
