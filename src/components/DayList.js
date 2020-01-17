c63990250e6b185004f309e22a5004216d58b10a
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-17 13:30:10 -0500

    refactored to send the setDay with the value of the day

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,10 +5,9 @@ export default function DayList(props) {
 
   let dayList = props.days.map(day => (
         <DayListItem key={day.id}
-                     name={day.name}
                      spots={day.spots}
                      selected={day.name === props.day}
-                     setDay={props.setDay}/>
+                     setDay={() => props.setDay(day.name)}/>
   ));
 
   return (
