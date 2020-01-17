60958fb875a1f7b3fc61fb16a42a7aa4dbf5a924
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-17 15:48:38 -0500

    added sty;es and created a component file for the Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import 'components/Appointment/styles.scss';
+
+export default function Application(props) {
+
+  return (
+    <article className="appointment">
+    </article>)
+
+}
