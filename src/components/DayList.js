a0ea4916cc8d0db86ef787fd41867593e6e7f1d0
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-17 15:47:14 -0500

    nothing changed

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,11 +4,14 @@ import DayListItem from 'components/DayListItem';
 export default function DayList(props) {
 
   let dayList = props.days.map(day => (
-        <DayListItem key={day.id}
-                     spots={day.spots}
-                     selected={day.name === props.day}
-                     setDay={() => props.setDay(day.name)}/>
+    <DayListItem key={day.id}
+                 name={day.name}
+                 spots={day.spots}
+                 selected={day.name === props.day}
+                 setDay={() => props.setDay(day.name)}/>
   ));
+
+  console.log(dayList);
 
   return (
     <ul>
