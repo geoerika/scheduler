0e55b43fb90cbea194c5f2b44ab35fe35de7545e
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-21 11:08:41 -0500

    added stories for Form Edit

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -187,4 +188,13 @@ storiesOf("Appointment", module)
       onClose={action("onClose")}
     />
   )
-
+  .add("Form Edit", () =>
+    <Form
+      name="Archie Cohen"
+      interviewers={interviewers}
+      interviewer={1}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+      setInterviewer={action("setInterviewer")}
+    />
+  )
