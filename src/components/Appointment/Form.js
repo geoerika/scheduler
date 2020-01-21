7e1cc88d41eea3dca26242b01aaeb43177c1756f
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-21 11:29:20 -0500

    inserted useState

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,6 +4,10 @@ import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
 
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -13,10 +17,11 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name? props.name : ""}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
