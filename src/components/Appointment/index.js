f8406dd7e12263148f5aa60fa07a5e864fb822dc
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-21 20:15:58 -0500

    included conditional rendering of the Show or empty components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,13 +7,12 @@ import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
 
-  let showEmpty = () => {
-    return props.interview ? <Show interview={props.interview}/> : <Empty/>;
-  }
-
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? (<Show interview={props.interview}/>) : (<Empty/>) }
+      { props.interview ?
+        (<Show interview={props.interview}/>) :
+        (<Empty/>)
+      }
     </article>)
 }
