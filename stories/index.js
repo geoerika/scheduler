31f4d5341d305998cd5d914246c3478e787000da
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-21 20:35:11 -0500

    included story for Appointment booked

--- stories/index.js
+++ stories/index.js
@@ -204,11 +204,19 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   )
-  .add("Appointment Empty", () => (
+  .add("Appointment Empty", () =>
     <Fragment>
       <Appointment id={1} time="12pm" />
       <Appointment id="last" time="1pm" />
     </Fragment>
-))
-
-
+  )
+  .add("Appointment Booked", () =>
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  )
