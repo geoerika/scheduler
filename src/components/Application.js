6e04205e333c769f058efe73f66683c369a2167a
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-22 14:04:10 -0500

    added appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,6 +21,62 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Anna Gables",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png"
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "9am",
+    interview: {
+      student: "Jerry McGuire",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg"
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "11am",
+    interview: {
+      student: "Tom Petty",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  }
+];
+
+
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
