116db366ff45c077f980e650803998c8a87e21b9
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-23 21:00:37 -0500

    added useEffect to fetch days with axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import DayList from './DayList';
 import Appointment from 'components/Appointment/index';
 
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -80,7 +63,22 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState("");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8001/api/days')
+      .then((response) => {
+        console.log(response);
+        setDays(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        console.log(error.response.data);
+      });
+  }, []);
 
   let appointmentList = appointments.map(appointment => {
     return <Appointment
