a006973f41b86d0d8af08e6f66292ebf2d0daa58
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-28 23:03:30 -0500

    retrieved appointments for each day

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,63 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DayList from './DayList';
 import Appointment from 'components/Appointment/index';
+import { getAppointmentsForDay } from '../helpers/selectors';
 
 import "components/Application.scss";
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     interview: {
-//       student: "Anna Gables",
-//       interviewer: {
-//         id: 3,
-//         name: "Mildred Nazir",
-//         avatar: "https://i.imgur.com/T2WwVfS.png"
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Jerry McGuire",
-//       interviewer: {
-//         id: 5,
-//         name: "Sven Jones",
-//         avatar: "https://i.imgur.com/twYrpay.jpg"
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//     interview: {
-//       student: "Tom Petty",
-//       interviewer: {
-//         id: 2,
-//         name: "Tori Malcolm",
-//         avatar: "https://i.imgur.com/Nmx0Qxo.png"
-//       }
-//     }
-//   }
-// ];
 
 export default function Application(props) {
 
@@ -76,7 +22,7 @@ export default function Application(props) {
       Promise.resolve(
         axios
           .get('http://localhost:8001/api/days')
-          // .then(response => setDays(response.data))
+          // .then(response => setDays(response.data)) // old code before making appointments call
           .catch((error) => {
             console.log(error.response.status);
             console.log(error.response.headers);
@@ -97,21 +43,17 @@ export default function Application(props) {
     });
   }, []);
 
-  // let appointmentList = appointments.map(appointment => {
-  //   return <Appointment
-  //             key={appointment.id}
-  //             {...appointment}
-  //           />
-  // });
+  let dayAppointments = getAppointmentsForDay(state, state.day);
 
-  let appointmentList = [];
-  for (const appointment in state.appointments) {
-    appointmentList.push(
-      <Appointment
-        key={state.appointments[appointment].id}
-        {...state.appointments[appointment]}
-      />)
-  }
+  let appointmentList = dayAppointments.map(appointment => {
+    return <Appointment
+              key={appointment.id}
+              {...appointment}
+            />
+  });
+
+
+  getAppointmentsForDay(state, state.day)
 
   return (
     <main className="layout">
