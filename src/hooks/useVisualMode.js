4ef0a0f20755e4a5d04b831ad6a49132a216a7ab
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-29 18:42:28 -0500

    implemented transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 export default function useVisualMode(initialMode) {
+
   const [mode, setMode] = useState(initialMode);
 
-  return { mode };
+  function transition(newMode) {
+    setMode(newMode);
+  }
+
+  return { mode, transition };
 }
