56457513a33486e198100b4747ae9559bf876daf
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-29 19:40:00 -0500

    implemented replacing mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
+// seeting mode for appointment component
 export default function useVisualMode(initialMode) {
 
   const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
 
-  function transition(newMode) {
-    setHistory([...history, newMode]);
+  //transitions to the next mode
+  function transition(newMode, replace = false) {
+    // we skip adding the new mode to history if we want to go back two modes
+    if (!replace) {
+      setHistory([...history, newMode]);
+    }
     setMode(newMode);
   }
 
+  // appointment comp goes back to the previous mode
   function back() {
     if (history.length > 1) {
       history.pop();
