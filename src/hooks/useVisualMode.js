7534c072c1dae2bc8c3feb15919096459bb7b34f
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-29 19:13:57 -0500

    added back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,10 +3,17 @@ import { useState } from "react";
 export default function useVisualMode(initialMode) {
 
   const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
 
   function transition(newMode) {
+    setHistory([...history, newMode]);
     setMode(newMode);
   }
 
-  return { mode, transition };
+  function back() {
+    history.pop();
+    setMode(history[history.length - 1]);
+  }
+
+  return { mode, transition, back };
 }
