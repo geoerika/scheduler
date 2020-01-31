74835c75322a0d5a04666dda61bc5aa6437026d4
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-31 16:57:47 -0500

    added useVisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,17 +4,26 @@ import 'components/Appointment/styles.scss';
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
 
-  console.log(props);
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      { props.interview ?
-        (<Show student={props.interview.student} interviewer={props.interview.interviewer.name}/>) :
-        (<Empty/>)
-      }
+      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>)
 }
