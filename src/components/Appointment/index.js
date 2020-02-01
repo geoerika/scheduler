be50ed9ec1ce0a1bd08576afc1af2fc0d103ca03
Erika Szabo <erika.szabo.geo@gmail.com>
2020-01-31 19:41:34 -0500

    added conditional views in appointment component, with transitioning to a form when + button pressed

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,26 +4,32 @@ import 'components/Appointment/styles.scss';
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
 
-  const { mode, transition, back } = useVisualMode(
+  const { mode, transition } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  console.log('mode: ', mode);
 
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === EMPTY && (<Empty onAdd={() => transition(CREATE)} />) }
       {mode === SHOW && (
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          interviewer={props.interview.interviewer.name}
         />
       )}
+      {mode === CREATE && <Form interviewers={[]} />}
+
     </article>)
 }
