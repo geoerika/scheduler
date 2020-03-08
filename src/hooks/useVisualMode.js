2b1f5847339378d83ed43c7ebd2933eab0178f03
Erika Szabo <erika.szabo.geo@gmail.com>
2020-03-07 22:24:45 -0500

    deleted console.logs

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -8,8 +8,6 @@ export default function useVisualMode(initialMode) {
 
   //transitions to the next mode
   function transition(newMode, replace = false) {
-    console.log('history in transition: ', history);
-    console.log('mode in transition: ', mode);
     // we skip adding the new mode to history if we want to go back two modes
     if (!replace) {
       setHistory(prev => ([...prev, newMode]));
@@ -19,9 +17,6 @@ export default function useVisualMode(initialMode) {
 
   // appointment comp goes back to the previous mode
   function back() {
-    console.log('history in back: ', history);
-    console.log('mode in back: ', mode);
-
     if (history.length > 1) {
       history.pop();
       setHistory(history);
